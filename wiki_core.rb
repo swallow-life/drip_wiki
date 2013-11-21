8ea533145f568bd560306f92b90980fa892b3e09
mshk_stt <mshk_stt@yahoo.co.jp>
2013-11-22 01:42:54 +0900

    separate erb/html template.

--- wiki_core.rb
+++ wiki_core.rb
@@ -9,6 +9,12 @@ require "rdiscount"
 helpers do
 	include Rack::Utils
 	alias_method :h, :escape_html
+
+	def unescape_slash(str) 
+		str.gsub(/&#x2F;/, "/")
+	end
+	alias_method :us, :unescape_slash
+
 end
 
 
@@ -24,92 +30,32 @@ end
 get '/wiki/list' do
 	#wikiページの一覧を表示する
 	list = settings.drip.head(10)
-	wiki_names = list.map do |elem|
+	@wiki_names = list.map do |elem|
 		elem[2] 
 	end
-	wiki_names.uniq!
-
-	partial_html = ""
-	wiki_names.each do |name|
-  		partial_html << "<a href='/wiki/#{name}'>#{name}</a><br />"	
-	end
-
-RHTML = <<EOS
-	<html>
-		<head>
-			<title>wiki</title>
-		  	<meta name="viewport" content="width=320" />
-		</head>
-	  	<body>
-	  		#{partial_html}
-	  	</body>
-	</html>
-EOS
+	@wiki_names.uniq!
+	erb :wiki_list
 end
 
 get '/wiki/history' do
 	#wikiページの更新履歴を表示する
-	list = settings.drip.head(10)
-	partial_html = ""
-	list.each do |elem|
-		key,_,name = elem
-		edit_time = Time.at(key / 1000000)
-	  	partial_html << "<a href='/wiki/#{name}'>#{name}</a> [#{edit_time.strftime("%Y/%m/%d %X")}]<br />"
-	end
-	
-RHTML = <<EOS
-	<html>
-		<head>
-			<title>wiki</title>
-		  	<meta name="viewport" content="width=320" />
-		</head>
-	  	<body>
-	  		#{partial_html}
-	  	</body>
-	</html>
-EOS
+	@list = settings.drip.head(10)
+	erb :wiki_history
 end
 
 get '/wiki/:name' do |name|
-
+	@name = name
 	edit = params[:edit]
 	#該当のwikiページを表示する
 	drip = settings.drip
-	_, v = drip.head(1, name)[0]
-	if v and not edit
-		contents = markdown(h v)
-
-RHTML = <<EOS
-	<html>
-		<head>
-			<title>wiki</title>
-		  	<meta name="viewport" content="width=320" />
-		</head>
-	  	<body>
-	  		<div>#{contents}</div>
-	  		<a href="/wiki/#{name}?edit=true">編集</a>
-		</body>
-	</html>
-EOS
-		return RHTML
-
+	_, @value = drip.head(1, name)[0]
+	if @value and not edit
+		@contents = markdown(us h @value)
+		return erb :wiki_page
 	end
 	#まだ存在しない場合はwikiページ作成用のページを表示する
-	button_name = !edit ? "ページを作成" : "ページを編集"
-RHTML = <<EOS
-	<html>
-		<head>
-			<title>wiki</title>
-		  	<meta name="viewport" content="width=320" />
-		</head>
-	  	<body>
-	    	<form method='post' action="/wiki/#{name}">
-			   	<textarea name="contents" rows="8" cols="80">#{v}</textarea><br />
-			   	<input type='submit' value='#{button_name}' />
-	    	</form>
-		</body>
-	</html>
-EOS
+	@button_name = !edit ? "ページを作成" : "ページを編集"
+	erb :wiki_edit
 end
 
 
